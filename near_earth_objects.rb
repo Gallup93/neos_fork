0a2d0abaef516ea28201c91adc09ce0b16aeffd8
Michael Gallup <Gallup93@icloud.com>
2020-06-27 11:08:11 -0600

    Moved work from unforked clone to a forked clone repository

--- near_earth_objects.rb
+++ near_earth_objects.rb
@@ -1,37 +1,45 @@
 require 'faraday'
 require 'figaro'
 require 'pry'
-# Load ENV vars via Figaro
-Figaro.application = Figaro::Application.new(environment: 'production', path: File.expand_path('../config/application.yml', __FILE__))
-Figaro.load
-
 class NearEarthObjects
-  def self.find_neos_by_date(date)
+  def initialize(date)
+    # Load ENV vars via Figaro
+    Figaro.application = Figaro::Application.new(environment: 'production', path: File.expand_path('../config/application.yml', __FILE__))
+    Figaro.load
+
     conn = Faraday.new(
       url: 'https://api.nasa.gov',
       params: { start_date: date, api_key: ENV['nasa_api_key']}
     )
     asteroids_list_data = conn.get('/neo/rest/v1/feed')
+    @parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
+  end
 
-    parsed_asteroids_data = JSON.parse(asteroids_list_data.body, symbolize_names: true)[:near_earth_objects][:"#{date}"]
-
-    largest_astroid_diameter = parsed_asteroids_data.map do |astroid|
-      astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
+  def get_largest_asteroid_diameter
+    @parsed_asteroids_data.map do |asteroid|
+      asteroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i
     end.max { |a,b| a<=> b}
+  end
 
-    total_number_of_astroids = parsed_asteroids_data.count
-    formatted_asteroid_data = parsed_asteroids_data.map do |astroid|
+  def get_total_number_of_asteroids
+    @parsed_asteroids_data.count
+  end
+
+  def get_formatted_asteroid_data
+    @parsed_asteroids_data.map do |asteroid|
       {
-        name: astroid[:name],
-        diameter: "#{astroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
-        miss_distance: "#{astroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
+        name: asteroid[:name],
+        diameter: "#{asteroid[:estimated_diameter][:feet][:estimated_diameter_max].to_i} ft",
+        miss_distance: "#{asteroid[:close_approach_data][0][:miss_distance][:miles].to_i} miles"
       }
     end
+  end
 
+  def get_formatted_neos_data
     {
-      astroid_list: formatted_asteroid_data,
-      biggest_astroid: largest_astroid_diameter,
-      total_number_of_astroids: total_number_of_astroids
+      asteroid_list: get_formatted_asteroid_data,
+      biggest_asteroid: get_largest_asteroid_diameter,
+      total_number_of_asteroids: get_total_number_of_asteroids
     }
   end
 end
--- near_earth_objects_test.rb
+++ near_earth_objects_test.rb
@@ -4,8 +4,32 @@ require 'pry'
 require_relative 'near_earth_objects'
 
 class NearEarthObjectsTest < Minitest::Test
+  def setup
+    @neos_test_date = NearEarthObjects.new('2019-03-30')
+  end
+
+  def test_initialization
+    assert_instance_of NearEarthObjects, @neos_test_date
+  end
+
   def test_a_date_returns_a_list_of_neos
-    results = NearEarthObjects.find_neos_by_date('2019-03-30')
-    assert_equal '(2019 GD4)', results[:astroid_list][0][:name]
+    results = @neos_test_date.get_formatted_neos_data
+    assert_equal '(2019 GD4)', results[:asteroid_list][0][:name]
+  end
+
+  def test_get_largest_asteroid_diameter
+    assert_equal 10233, @neos_test_date.get_largest_asteroid_diameter
+  end
+
+  def test_get_total_number_of_asteroids
+    assert_equal 12, @neos_test_date.get_total_number_of_asteroids
+  end
+
+  def test_get_formatted_asteroid_data
+    expected1 = {:name=>"(2019 GD4)", :diameter=>"61 ft", :miss_distance=>"911947 miles"}
+    expected2 = {:name=>"(2019 UZ)", :diameter=>"51 ft", :miss_distance=>"37755577 miles"}
+
+    assert_equal expected1, @neos_test_date.get_formatted_asteroid_data.first
+    assert_equal expected2, @neos_test_date.get_formatted_asteroid_data.last
   end
 end
